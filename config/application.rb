92929da0488c57e767fdac81bfd3950515ca42a3
malaikaMI <malaikamic@gmail.com>
2018-10-23 12:35:35 -0400

    Saved assets

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module RecipeBox
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     # config/application.rb
-    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
